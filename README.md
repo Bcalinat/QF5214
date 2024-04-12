e1fa7450bfc16f1994a95bb0e4b1faef15d9b6bc
bcalinat <bcalinat@outlook.com>
2024-04-13 00:59:41 +0800

    add schema

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # QF5214
+## Schema
+### Fact Table
+1. Company Data
+
+### Dimension Tables
+2. search_popularity
+3. yahoo_esg_score
+4. sentiment
+5. macro_index
+6. industry_summary_daily
+7. industry_summary_monthly
+8. industry_summary_yearly
+9. industry_index_summary
---
+++ qf.session.sql
@@ -0,0 +1,13 @@
+SHOW CREATE TABLE stock_price_features;
+SHOW CREATE TABLE macro_index;
+SHOW CREATE TABLE industry_summary_daily;
+
+SHOW CREATE TABLE industry_summary_monthly;
+
+SHOW CREATE TABLE industry_summary_yearly;
+SHOW CREATE TABLE search_popularity;
+
+SHOW CREATE TABLE yahoo_esg_score;
+SHOW CREATE TABLE sentiment;
+
+SHOW CREATE TABLE industry_index_summary;
